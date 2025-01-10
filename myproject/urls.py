8fd8ddd54157c267b8194b67126a14e206b214b8
iamchouty <quocchau4729@gmail.com>
2025-01-10 14:16:08 +0700

    Fixed

--- myproject/urls.py
+++ myproject/urls.py
@@ -21,10 +21,9 @@ from . import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('admin/', admin.site.urls),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', views.homepage, name='homepage'),
 ]
 
